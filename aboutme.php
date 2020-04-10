2db69d1f2aaae9e4494472055b74bf90a2852e9c
zaidanfadhil <55734083+zaidanfadhil@users.noreply.github.com>
2020-04-10 19:29:59 +0700

    Add files via upload

--- aboutme.php
+++ aboutme.php
@@ -2,7 +2,5 @@
     echo' Nama : zaidan fadhil <br>';
     echo' Kelas : XI RPL 1 <br>';
     echo "SMK Taruna Bhakti";
-    echo "jl.kemang rt03 rw010";
-    echo' Email : zaitarou555@gmail.com<br>';
     echo' No Telpon : 085892923686 <br>'; 
 ?>
